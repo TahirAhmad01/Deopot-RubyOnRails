9e497058aca30c453d759c276296312bd30703d2
Tahir Ahmad <tahirahmad0234@gmail.com>
2024-01-22 18:00:37 +0600

    :Sending Confirmation Emails

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -61,4 +61,18 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # email configaration
+  Rails.application.configure do
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "domain.of.sender.net",
+      authentication: "plain",
+      user_name: "dave",
+      password: "secret",
+      enable_starttls_auto: true
+    }
+  end
 end
