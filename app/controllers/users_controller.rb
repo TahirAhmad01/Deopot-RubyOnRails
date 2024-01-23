2aaafe7697bf126355c83d57b7b86aa0bdb11013
Tahir Ahmad <tahirahmad0234@gmail.com>
2024-01-23 13:25:44 +0600

    Adding a Sidebar, More Administration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,13 +56,17 @@ class UsersController < ApplicationController
     @user.destroy
 
     respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
+      format.html { redirect_to users_url,
+                                notice: "User #{@user.name} deleted" }
       format.json { head :no_content }
     end
   end
 
-  private
+  rescue_from 'User::Error' do |exception|
+    redirect_to users_url, notice: exception.message
+  end
 
+  private
   # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
