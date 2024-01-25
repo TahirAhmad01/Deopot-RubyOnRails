9db3ad94202dd2df101496720e05c1969569fd83
Tahir Ahmad <tahirahmad0234@gmail.com>
2024-01-25 16:24:37 +0600

    Storing Support Requests from Our Mailbox

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,13 @@ Rails.application.routes.draw do
     post 'login' => :create
     delete 'logout' => :destroy
   end
-
-  get 'admin/index'
-  get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
+
+  # START_HIGLIGHT
+  resources :support_requests, only: %i[ index update ]
+  # END_HIGLIGHT
   resources :users
-  # resources :orders
-  # resources :line_items
-  # resources :carts
-  # root 'store#index', as: 'store_index'
   resources :products do
     get :who_bought, on: :member
   end
@@ -25,10 +22,4 @@ Rails.application.routes.draw do
     resources :carts
     root 'store#index', as: 'store_index', via: :all
   end
-
-  # Define your application routes per the DSL in
-  # https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
