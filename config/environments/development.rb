fa40a38638f5f57b43433f73a154e9bb94f7832a
Tahir Ahmad <tahirahmad0234@gmail.com>
2024-01-25 11:19:01 +0600

    Storing Support Requests from Our Mailbox

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -83,4 +83,5 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
   config.action_cable.disable_request_forgery_protection = true
+  config.active_storage.service = :local
 end
---
+++ db/migrate/20240125051457_create_support_requests.rb
@@ -0,0 +1,8 @@
+class CreateSupportRequests < ActiveRecord::Migration[7.1]
+  def change
+    create_table :support_requests do |t|
+
+      t.timestamps
+    end
+  end
+end
