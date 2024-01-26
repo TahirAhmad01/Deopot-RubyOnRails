44bd9cdd0c47f9e0c2028fd416163732a5fce87f
Tahir Ahmad <tahirahmad0234@gmail.com>
2024-01-26 11:14:54 +0600

    Responding with Rich Text

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   get 'admin' => 'admin#index'
   controller :sessions do
-    get 'login' => :new
+    get  'login' => :new
     post 'login' => :create
     delete 'logout' => :destroy
   end
   get 'sessions/create'
   get 'sessions/destroy'
+
 
   # START_HIGLIGHT
   resources :support_requests, only: %i[ index update ]
