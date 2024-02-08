5055d3974e50aa4f35e92727f635aef53fb6b772
Tahir Ahmad <tahirahmad0234@gmail.com>
2024-02-08 11:46:04 +0600

    product test code added

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,8 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@rails/actioncable", to: "actioncable.esm.js"
 pin_all_from "app/javascript/channels", under: "channels"
+pin "lit" # @3.1.1
+pin "@lit/reactive-element", to: "@lit--reactive-element.js" # @2.0.3
+pin "lit-element/lit-element.js", to: "lit-element--lit-element.js.js" # @4.0.3
+pin "lit-html" # @3.1.1
+pin "lit-html/is-server.js", to: "lit-html--is-server.js.js" # @3.1.1
